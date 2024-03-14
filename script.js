e0432f75d663622ff3721ebf405006afec4c9a9a
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-03-14 09:18:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ var rebirthcount = 0;
 function mainBttnClick() {
   money += boost*rebirthboost;document.getElementById('moneydisplay').innerHTML = '$' + money.toString();
 }
+function boostIncreaseBttnClick() {
+  if (money > boost * diffiMultForBoost) {
+    money -= boost * diffiMultForBoost;
+    boost = boost * 2;
+    diffiMultForBoost += diffiDirectionOne;
+    diffiDirectionOne *= 2;
+    if (diffiMultForBoost > 130) {
+        diffiDirectionOne = -2
+    }
+    document.getElementById('moneydisplay').innerHTML = '$' + money.toString();
+    document.getElementById('boostIncreaseBttn').innerHTML = '$' + (boost * diffiMultForBoost).toString()
+  }
+}
